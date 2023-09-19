4af0717fb3f687aa89b50af14182f4ac92bc859f
vishnusureshperumbavoor <vishnusureshperumbavoor@gmail.com>
2023-09-19 19:14:56 +0530

    added google collab

--- bot.mjs
+++
@@ -1,9 +0,0 @@
-import { client } from "@gradio/client";
-
-async function getResponse(userMessage) {
-  const app = await client(
-    "https://vishnusureshperumbavoor-vspbot-falcon-langchain.hf.space/"
-  );
-  const result = await app.predict("/predict", [userMessage]);
-  return result.data;
-}
